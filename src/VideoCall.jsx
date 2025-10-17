12e3e1a3d6c2730433ee923e02be73a6bd4bd0d2
Yasser <yassercomunica@gmail.com>
2025-10-16 21:19:02 -0600

    tesr

--- src/VideoCall.jsx
+++ src/VideoCall.jsx
@@ -24,31 +24,7 @@ export default function VideoCall({ roomId }) {
 
         // 2️⃣ Crear RTCPeerConnection
         const pc = new RTCPeerConnection({
-         iceServers: [
-          {
-            urls: "stun:stun.relay.metered.ca:80",
-          },
-          {
-            urls: "turn:global.relay.metered.ca:80",
-            username: "6fe6e5a89fa11f91a4ac00e3",
-            credential: "ymXWPsHIeRXxeV8J",
-          },
-          {
-            urls: "turn:global.relay.metered.ca:80?transport=tcp",
-            username: "6fe6e5a89fa11f91a4ac00e3",
-            credential: "ymXWPsHIeRXxeV8J",
-          },
-          {
-            urls: "turn:global.relay.metered.ca:443",
-            username: "6fe6e5a89fa11f91a4ac00e3",
-            credential: "ymXWPsHIeRXxeV8J",
-          },
-          {
-            urls: "turns:global.relay.metered.ca:443?transport=tcp",
-            username: "6fe6e5a89fa11f91a4ac00e3",
-            credential: "ymXWPsHIeRXxeV8J",
-          },
-        ],
+          iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
         });
         pcRef.current = pc;
 
